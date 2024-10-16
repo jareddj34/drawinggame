e60491194d6da3c5e7b87985748d5836b1a2deac
jareddj34 <jareddj34@gmail.com>
2024-10-16 12:39:27 -0400

    final draft

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const canvas = document.getElementById("circleCanvas");
 const ctx = canvas.getContext("2d");
 const result = document.getElementById("result");
+const highScoreText = document.getElementById("highScore");
 const resetButton = document.getElementById("resetButton");
+const goButton = document.getElementById("goButton");
 
 ctx.strokeStyle = "BADA55";
 ctx.lineJoin = "round";
@@ -18,11 +20,24 @@ let direction = true;
 let tooClose = false;
 let isBad = false;
 let highScore = 0;
+let goneAround = false;
+let fullRound = false;
+let previousPoint = null;
+let previousTime = null;
+
+let drawingSound = new Audio("drawing.mp3");
 
 let path = [];
 
 window.onload = () => {
-    drawCenterDot();
+    //drawCenterDot();
+    // ctx.font = "50px comic sans ms";
+    // ctx.fillText(
+    //     "Draw a circle",
+    //     canvas.width / 2 - 160,
+    //     canvas.height / 2 - 30
+    // );
+    // ctx.fillText("around this", canvas.width / 2 - 140, canvas.height / 2 + 55);
 };
 
 function drawCenterDot() {
@@ -36,13 +51,53 @@ function drawCenterDot() {
     ctx.closePath();
 }
 
+goButton.addEventListener("click", () => {
+    goButton.style.display = "none"; // Hide the button
+    startGame();
+});
+
+function startGame() {
+    // Initialize game state and start drawing
+    drawCenterDot();
+    ctx.font = "50px comic sans ms";
+    ctx.fillText(
+        "Draw a circle",
+        canvas.width / 2 - 160,
+        canvas.height / 2 - 30
+    );
+    ctx.fillText("around this", canvas.width / 2 - 140, canvas.height / 2 + 55);
+}
+
 function draw(e) {
-    if (!isDrawing) return;
+    if (!isDrawing || goButton.style.display !== "none") return;
 
     //function stuff
     const x = e.offsetX;
     const y = e.offsetY;
-    console.log("current x and y", x, y);
+
+    const currentTime = Date.now();
+    if (previousPoint && previousTime) {
+        const distance = Math.sqrt(
+            Math.pow(x - previousPoint.x, 2) + Math.pow(y - previousPoint.y, 2)
+        );
+        const timeElapsed = currentTime - previousTime;
+        const speed = distance / timeElapsed;
+        const invertedSpeed = 1 / speed;
+
+        // Adjust stroke size based on speed
+        const minStrokeSize = 1;
+        const maxStrokeSize = 15;
+        const strokeSize = Math.min(
+            maxStrokeSize,
+            Math.max(minStrokeSize, invertedSpeed * 8)
+        );
+
+        ctx.lineWidth = strokeSize;
+    }
+
+    previousPoint = { x, y };
+    previousTime = currentTime;
+
     path.push({ x, y });
     const score = evaluateCircle(path);
 
@@ -53,19 +108,28 @@ function draw(e) {
         redValue = 0;
     } else {
         //Completed circle
-        if (Math.abs(x - startX) <= 5 && Math.abs(y - startY) <= 5) {
+        if (
+            Math.abs(x - startX) <= 10 &&
+            Math.abs(y - startY) <= 10 &&
+            goneAround &&
+            path.length > 40
+        ) {
+            console.log(path.length);
+            result.innerText = `${score.toFixed(1)}%`;
             if (score > highScore) {
+                if (score > 90) {
+                    angelSound();
+                }
+
                 highScore = score;
-                result.innerText = `You got a new high score of ${score.toFixed(
-                    2
-                )}%`;
+                goodSound();
+                highScoreText.innerText = `New best score`;
+                highScoreText.classList.add("glow"); // Add the glow effect
             } else {
-                result.innerText = `You have completed the circle with a score of ${score.toFixed(
-                    2
-                )}%`;
+                highScoreText.innerText = `Best: ${highScore.toFixed(1)}%`;
             }
             isDrawing = false;
-            goodSound();
+
             return;
         }
         greenValue = Math.min(
@@ -84,14 +148,27 @@ function draw(e) {
 
     if (tooClose) {
         result.innerText = `Too close to the dot!`;
+        ctx.fillText(
+            "Too close to the dot!",
+            canvas.width / 2 - 210,
+            canvas.height / 2 + 55
+        );
     } else {
-        result.innerText = `Your circle score: ${score.toFixed(2)}%`;
+        result.innerText = `${score.toFixed(1)}%`;
     }
 
     if (isBad) {
         result.innerText = `You are NOT drawing a circle well!`;
     } else {
-        result.innerText = `Your circle score: ${score.toFixed(2)}%`;
+        // Clear the area where the text will be drawn
+        // ctx.clearRect(canvas.width / 2 - 150, canvas.height / 2, 500, 500);
+
+        // ctx.fillText(
+        //     `${score.toFixed(2)}%`,
+        //     canvas.width / 2 - 120,
+        //     canvas.height / 2 + 55
+        // );
+        result.innerText = `${score.toFixed(1)}%`;
     }
 }
 
@@ -100,25 +177,34 @@ function clearCanvas() {
 }
 
 canvas.addEventListener("mousedown", (e) => {
+    if (goButton.style.display !== "none") return;
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
     startX = e.offsetX;
     startY = e.offsetY;
+    goneAround = false;
+    fullRound = false;
+
+    highScoreText.classList.remove("glow");
 
     console.log("start x and y", startX, startY);
 
     //game function stuff
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear previous drawings
+    //clear the highscoretext
+    highScoreText.innerText = ``;
     path = [];
     path.push({ x: lastX, y: lastY });
 
     drawCenterDot();
+    drawSound();
 });
 
 canvas.addEventListener("mousemove", draw);
 
 canvas.addEventListener("mouseup", () => {
     isDrawing = false;
+    stopDrawSound();
 });
 
 function evaluateCircle(path) {
@@ -146,6 +232,11 @@ function evaluateCircle(path) {
         const dy = point.y - center.y;
         const radius = Math.sqrt(dx * dx + dy * dy);
         totalDeviation += Math.abs(radius - startRadius);
+
+        if (point.y > center.y && !goneAround) {
+            goneAround = true;
+            console.log("gone around: " + goneAround);
+        }
     }
 
     //console.log("total deviation", totalDeviation);
@@ -164,12 +255,6 @@ function evaluateCircle(path) {
     return Math.max(0, score); // Ensure score is not negative
 }
 
-resetButton.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    result.innerText = "";
-    drawCenterDot();
-});
-
 function wrongSound() {
     let audio = new Audio("wrong.mp3");
     audio.play();
@@ -177,5 +262,20 @@ function wrongSound() {
 
 function goodSound() {
     let audio = new Audio("good.wav");
+    audio.play();
+}
+
+function drawSound() {
+    drawingSound.loop = true; // Set the sound to loop
+    drawingSound.play(); // Start playing the sound
+}
+
+function stopDrawSound() {
+    drawingSound.pause(); // Pause the sound when drawing stops
+    drawingSound.currentTime = 0; // Reset sound to the start for the next drawing
+}
+
+function angelSound() {
+    let audio = new Audio("angel.mp3");
     audio.play();
 }
